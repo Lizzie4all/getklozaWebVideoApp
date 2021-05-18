b5b041d59ab948ded048d9b156fd609df80b8ad4
[MustaphaAhmd724] <mustaphaahmed724@gmail.com>
2021-05-18 10:46:09 +0100

    b4 changes

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@ const peers = {}
 var peer = new Peer(undefined, {
     path: '/peerjs', 
     host: '/',
-    port: '443'
+    port: '3030'
 });
  
 let myVideoStream
@@ -71,7 +71,7 @@ $('html').keydown((e) => {
 })
 
 socket.on('createMessage', message => {
-    $('ul').append(`<li class="message"><b>user:  </b>${message}</li>`)
+  $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
     scrollToBottom()
 })
 
